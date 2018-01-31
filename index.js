0ede6cae8c8d2319687b118e124121835e4c22d7
erha19 <faterrole@gmail.com>
2018-02-01 00:02:47 +0800

    Update config.

--- index.js
+++ index.js
@@ -33,15 +33,15 @@ const runTask = async () => {
   await git.clone(`${root[0].repo} -b ${root[0].branch} ${configs.rootPath}/${root[0].directory}tmpelate`)
   console.log(`rm -rf ${configs.rootPath}/${root[0].directory}tmpelate/.git`)
   await rm(`-rf ${configs.rootPath}/${root[0].directory}tmpelate/.git`)
-  console.log(`cd ${configs.rootPath}/${root[0].directory}tmpelate`)
-  process.chdir(`${configs.rootPath}/${root[0].directory}tmpelate`)
-  console.log(`=> npm install on ${configs.rootPath}/${root[0].directory}tmpelate`)
-  await npm('install')
-  console.log(`=> ./node_modules/.bin/hexo g`)
-  await simpleExec('./node_modules/.bin/hexo g')
-  console.log(`=> mv public/ .deploy/`)
-  await mv(`public/ .deploy/`)
-  process.chdir(`.deploy`)
+  // console.log(`cd ${configs.rootPath}/${root[0].directory}tmpelate`)
+  // process.chdir(`${configs.rootPath}/${root[0].directory}tmpelate`)
+  // console.log(`=> npm install on ${configs.rootPath}/${root[0].directory}tmpelate`)
+  // await npm('install')
+  // console.log(`=> ./node_modules/.bin/hexo g`)
+  // await simpleExec('./node_modules/.bin/hexo g')
+  // console.log(`=> mv public/ .deploy/`)
+  // await mv(`public/ .deploy/`)
+  // process.chdir(`.deploy`)
   for(let i in configs.gits) {
     if (/^http(s)?/.test(configs.gits[i].repo)) {
       console.log(`=> git clone ${configs.gits[i].repo} -b ${configs.gits[i].branch} ${configs.gits[i].directory}`)
@@ -49,15 +49,15 @@ const runTask = async () => {
       await rm(`-rf ${configs.gits[i].directory}/.git`)
     }
     else {
-      await mkdir(configs.gits[i].repo)
-      await cp(`-R ${path.join(ROOT, configs.gits[i].repo)}/* ${path.join(ROOT, configs.rootPath, `${root[0].directory}tmpelate`, '.deploy', configs.gits[i].repo)}`)
+      await mkdir(`${path.join(ROOT, configs.rootPath,`${root[0].directory}tmpelate`,configs.gits[i].repo)}`)
+      await cp(`-R ${path.join(ROOT, configs.gits[i].repo)}/* ${path.join(ROOT, configs.rootPath, `${root[0].directory}tmpelate`, configs.gits[i].repo)}`)
     }
   }
   console.log(`=> rm -rf ${path.join(ROOT, configs.rootPath, root[0].directory)}`)
   await rm(`-rf ${path.join(ROOT, configs.rootPath, root[0].directory)}`)
   await mkdir(`${path.join(ROOT, configs.rootPath, root[0].directory)}`)
-  console.log(`=> mv .deploy/ ${path.join(ROOT, configs.rootPath, root[0].directory)}`)
-  await cp(`-R ${path.join(ROOT, configs.rootPath, `${root[0].directory}tmpelate`, '.deploy')}/* ${path.join(ROOT, configs.rootPath, root[0].directory)}/`)
+  console.log(`=> cp -R ${path.join(ROOT, configs.rootPath, `${root[0].directory}tmpelate`)}/* ${path.join(ROOT, configs.rootPath, root[0].directory)}/`)
+  await cp(`-R ${path.join(ROOT, configs.rootPath, `${root[0].directory}tmpelate`)}/ ${path.join(ROOT, configs.rootPath, root[0].directory)}/`)
   console.log('Successful Update!')
 }
 
@@ -70,4 +70,5 @@ const simpleExec = async (cmd) => {
   })
 }
 
-scheduleCronstyle();
+// scheduleCronstyle();
+runTask();
